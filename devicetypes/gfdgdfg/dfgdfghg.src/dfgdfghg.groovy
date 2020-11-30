49e5469627df2fd756ff722d6f40c9bfb1312f15
Gabriel Hajjar <gabriel+2@upwis.com>
2020-11-30 15:06:09 +0100

    tgdh

---
+++ devicetypes/gfdgdfg/dfgdfghg.src/dfgdfghg.groovy
@@ -0,0 +1,62 @@
+/**
+ *  dfgdfghg
+ *
+ *  Copyright 2020 Gabriel Hajjar
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ */
+metadata {
+	definition (name: "dfgdfghg", namespace: "gfdgdfg", author: "Gabriel Hajjar", cstHandler: true) {
+		capability "Alarm"
+		capability "Battery"
+		capability "Contact Sensor"
+
+		fingerprint mfr: "7894", prod: "6543", model: "1542", deviceJoinName: "gfddddddddd"
+	}
+
+
+	simulator {
+		// TODO: define status and reply messages here
+	}
+
+	tiles {
+		// TODO: define your main and details tiles here
+	}
+}
+
+// parse events into attributes
+def parse(String description) {
+	log.debug "Parsing '${description}'"
+	// TODO: handle 'alarm' attribute
+	// TODO: handle 'battery' attribute
+	// TODO: handle 'contact' attribute
+
+}
+
+// handle commands
+def off() {
+	log.debug "Executing 'off'"
+	// TODO: handle 'off' command
+}
+
+def strobe() {
+	log.debug "Executing 'strobe'"
+	// TODO: handle 'strobe' command
+}
+
+def siren() {
+	log.debug "Executing 'siren'"
+	// TODO: handle 'siren' command
+}
+
+def both() {
+	log.debug "Executing 'both'"
+	// TODO: handle 'both' command
+}
